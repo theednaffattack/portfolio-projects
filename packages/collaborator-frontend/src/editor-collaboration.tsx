12c4769995a9a4b90f582fc02587ee41351ce964
Eddie Naff <eddienaff@gmail.com>
2023-03-30 12:38:02 -0700

    Add auto embed and sticky note plugins

--- packages/collaborator-frontend/src/editor-collaboration.tsx
+++ packages/collaborator-frontend/src/editor-collaboration.tsx
@@ -13,6 +13,7 @@ import * as Y from "yjs";
 
 import { createWebsocketProvider } from "./collaboration";
 import ActionsPlugin from "./plugins/actions-plugin";
+import AutoEmbedPlugin from "./plugins/auto-embed-plugin";
 import CodeActionMenuPlugin from "./plugins/CodeActionMenuPlugin";
 import DragDropPaste from "./plugins/DragDropPastePlugin";
 import DraggableBlockPlugin from "./plugins/DraggableBlockPlugin";
@@ -21,6 +22,7 @@ import FloatingTextFormatToolbarPlugin from "./plugins/FloatingTextFormatToolbar
 import ImagesPlugin from "./plugins/images-plugin";
 import LinkPlugin from "./plugins/link-plugin";
 import PollPlugin from "./plugins/poll-plugin";
+import StickyPlugin from "./plugins/StickyPlugin";
 import TableCellActionMenuPlugin from "./plugins/TableActionMenuPlugin";
 import ToolbarPlugin from "./plugins/toolbar-plugin";
 import YouTubePlugin from "./plugins/you-tube-plugin";
@@ -60,61 +62,63 @@ export function EditorCollab() {
 
   return (
     <div className="editor-container">
-      <LexicalComposer initialConfig={initialConfig}>
-        <ToolbarPlugin />
-        <RichTextPlugin
-          contentEditable={
-            <div className="editor-scroller">
-              <div className="editor" ref={onRef}>
-                <ContentEditable className="edit-document ContentEditable__root" />
-              </div>
+      {/* <LexicalComposer initialConfig={initialConfig}> */}
+      <ToolbarPlugin />
+      <RichTextPlugin
+        contentEditable={
+          <div className="editor-scroller">
+            <div className="editor" ref={onRef}>
+              <ContentEditable className="edit-document ContentEditable__root" />
             </div>
-          }
-          placeholder={null}
-          ErrorBoundary={LexicalErrorBoundary}
-        />
-        <CollaborationPlugin
-          id="playground"
-          providerFactory={(id, yjsDocMap) => {
-            const doc = new Y.Doc();
-            yjsDocMap.set(id, doc);
+          </div>
+        }
+        placeholder={null}
+        ErrorBoundary={LexicalErrorBoundary}
+      />
+      <CollaborationPlugin
+        id="playground"
+        providerFactory={(id, yjsDocMap) => {
+          const doc = new Y.Doc();
+          yjsDocMap.set(id, doc);
 
-            const provider = createWebsocketProvider(id, yjsDocMap);
+          const provider = createWebsocketProvider(id, yjsDocMap);
 
-            return provider;
-          }}
-          cursorColor="pink"
-          username="boa constrictor"
-          // Optional initial editor state in case collaborative Y.Doc won't
-          // have any existing data on server. Then it'll user this value to populate editor.
-          // It accepts same type of values as LexicalComposer editorState
-          // prop (json string, state object, or a function)
-          initialEditorState={initialEditorState}
-          shouldBootstrap={true}
-        />
-        <DragDropPaste />
-        <AutoFocusPlugin />
-        <ClearEditorPlugin />
-        <ActionsPlugin isRichText={true} />
-        <ImagesPlugin />
-        <LinkPlugin />
-        <PollPlugin />
-        <YouTubePlugin />
-        {floatingAnchorElem && !isSmallWidthViewport ? (
-          <>
-            <DraggableBlockPlugin anchorElem={floatingAnchorElem} />
-            <CodeActionMenuPlugin anchorElem={floatingAnchorElem} />
-            <FloatingLinkEditorPlugin anchorElem={floatingAnchorElem} />
-            <TableCellActionMenuPlugin
-              anchorElem={floatingAnchorElem}
-              cellMerge={true}
-            />
-            <FloatingTextFormatToolbarPlugin anchorElem={floatingAnchorElem} />
-          </>
-        ) : (
-          ""
-        )}
-      </LexicalComposer>
+          return provider;
+        }}
+        cursorColor="pink"
+        username="boa constrictor"
+        // Optional initial editor state in case collaborative Y.Doc won't
+        // have any existing data on server. Then it'll user this value to populate editor.
+        // It accepts same type of values as LexicalComposer editorState
+        // prop (json string, state object, or a function)
+        initialEditorState={initialEditorState}
+        shouldBootstrap={true}
+      />
+      <DragDropPaste />
+      <AutoFocusPlugin />
+      <ClearEditorPlugin />
+      <ActionsPlugin isRichText={true} />
+      <ImagesPlugin />
+      <LinkPlugin />
+      <PollPlugin />
+      <YouTubePlugin />
+      <AutoEmbedPlugin />
+      <StickyPlugin />
+      {floatingAnchorElem && !isSmallWidthViewport ? (
+        <>
+          <DraggableBlockPlugin anchorElem={floatingAnchorElem} />
+          <CodeActionMenuPlugin anchorElem={floatingAnchorElem} />
+          <FloatingLinkEditorPlugin anchorElem={floatingAnchorElem} />
+          <TableCellActionMenuPlugin
+            anchorElem={floatingAnchorElem}
+            cellMerge={true}
+          />
+          <FloatingTextFormatToolbarPlugin anchorElem={floatingAnchorElem} />
+        </>
+      ) : (
+        ""
+      )}
+      {/* </LexicalComposer> */}
     </div>
   );
 }

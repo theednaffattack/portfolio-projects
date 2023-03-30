402db47fa6d54113a48a88d9168ea053bf73c12f
Eddie Naff <eddienaff@gmail.com>
2023-03-30 15:27:48 -0700

    Fix isNumber util

--- packages/utils/src/is-string.ts
+++ packages/utils/src/is-string.ts
@@ -2,6 +2,6 @@ export function isString(property: string | any): property is string {
   return typeof property === "string";
 }
 
-export function isNumber<T>(property: string | T): property is string {
+export function isNumber<T>(property: number | T): property is number {
   return !!property;
 }

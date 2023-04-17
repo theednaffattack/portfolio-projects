099c1500207d0f4931261051a06c1ea3a528ca42
Eddie Naff <eddienaff@gmail.com>
2023-04-17 14:59:43 -0700

    linkedin-front: Initial commit

---
+++ packages/linkedin-frontend/src/components/app.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque
+        consequatur vel nostrum et quas excepturi consequuntur libero iure, quo
+        accusamus ipsam magnam aliquid temporibus perferendis aliquam minus.
+        Nam, nobis eius!
+      </p>
+    </div>
+  );
+}
+
+export default App;

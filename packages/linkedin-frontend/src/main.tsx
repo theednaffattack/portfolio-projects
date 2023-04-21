5cfecaad43d7b98e21d70d6681753c3cd8713a4e
Eddie Naff <eddienaff@gmail.com>
2023-04-20 17:28:10 -0700

    Add react router to app

--- packages/linkedin-frontend/src/main.tsx
+++ packages/linkedin-frontend/src/main.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import App from "./components/app";
 import "./index.css";
+import { HomeUnauthenticated } from "./components/home-unauthenticated";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeUnauthenticated />,
+  },
+  {
+    path: "/home",
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );

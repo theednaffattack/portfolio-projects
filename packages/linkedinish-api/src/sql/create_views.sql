172a5779126da54365e822fda44c3e97e85e8d94
Eddie Naff <eddienaff@gmail.com>
2023-05-01 15:29:31 -0700

    Add 'USE' command to create view file

--- packages/linkedinish-api/src/sql/create_views.sql
+++ packages/linkedinish-api/src/sql/create_views.sql
@@ -1,5 +1,9 @@
+-- Specify Database to use
+USE linkedinish_api_dev;
+
+-- Create view to view permissions
 CREATE VIEW vw_user_organization_permissions AS
-	SELECT A.user_id, A.organization_id, C.slug as permission_slug
+	SELECT A.user_id, A.organization_id, C.permission_name
 	FROM users_roles AS A
 	LEFT JOIN roles_permissions AS B ON A.role_id = B.role_id
 	LEFT JOIN permissions AS C on B.permission_id = C.id;

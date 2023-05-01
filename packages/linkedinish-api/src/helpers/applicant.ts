5f8203a58c6618e76e150cb0bae497758fecff4c
Eddie Naff <eddienaff@gmail.com>
2023-04-30 21:24:23 -0700

    Add new helper applicant for use with token

---
+++ packages/linkedinish-api/src/helpers/applicant.ts
@@ -0,0 +1,18 @@
+import { createParamDecorator } from "type-graphql";
+import type { ContextType } from "~/types";
+import type { TokenTypes } from "../types/token-types-enum";
+import { AuthenticationError, AuthorizationError } from "../errors/all";
+
+type ApplicantArgs = {
+  type?: TokenTypes;
+};
+
+export function Applicant(args?: ApplicantArgs): ParameterDecorator {
+  return createParamDecorator<ContextType>(({ context }) => {
+    if (!context.applicant) throw new AuthenticationError();
+    if (args?.type && args?.type !== context.applicant.type)
+      throw new AuthorizationError();
+
+    return context.applicant;
+  });
+}

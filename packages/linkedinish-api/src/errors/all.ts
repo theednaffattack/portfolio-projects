a6c460e4c0135c2f0341921e2a2e902c2c479538
Eddie Naff <eddienaff@gmail.com>
2023-04-30 21:21:05 -0700

    Collect helper exports into one file

---
+++ packages/linkedinish-api/src/errors/all.ts
@@ -0,0 +1,6 @@
+export * from "./authentication-error";
+export * from "./authorization-error";
+export * from "./database-error";
+export * from "./internal-server-error";
+export * from "./token-error";
+export * from "./validation-error";

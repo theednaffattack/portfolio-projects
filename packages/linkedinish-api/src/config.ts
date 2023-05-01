1029bd79a2ace14ab7d4b463f7b06f0671300c6a
Eddie Naff <eddienaff@gmail.com>
2023-04-30 21:18:13 -0700

    Alter config fields to customize app settings

--- packages/linkedinish-api/src/config.ts
+++ packages/linkedinish-api/src/config.ts
@@ -1,31 +1,11 @@
-/*
- * MIT License
- *
- * Copyright (c) 2022-2023 Carlo Corradini
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 import convert from "convert";
 import type pino from "pino";
+import { address } from "ip";
+
 import type { Config } from "./types";
 import { env } from "./env";
+
+const ipAddress = address();
 
 export const config: Config = {
   app: "LinkedInish",
@@ -33,7 +13,7 @@ export const config: Config = {
     env: env.NODE_ENV,
   },
   server: {
-    host: env.HOST,
+    host: ipAddress, // env.HOST,
     port: env.PORT,
   },
   logger: {
@@ -42,11 +22,11 @@ export const config: Config = {
   database: {
     url: env.DATABASE_URL,
   },
-  ssh: {
-    username: env.SSH_USERNAME,
-    passphrase: env.SSH_PASSPHRASE,
-    privateKey: env.SSH_PRIVATE_KEY,
-  },
+  // ssh: {
+  //   username: env.SSH_USERNAME,
+  //   passphrase: env.SSH_PASSPHRASE,
+  //   privateKey: env.SSH_PRIVATE_KEY,
+  // },
   token: {
     algorithm: "RS256",
     expiration: convert(365, "d").to("s"),
@@ -55,9 +35,6 @@ export const config: Config = {
     publicKey: env.TOKEN_PUBLIC_KEY,
   },
   graphql: { path: "/graphql" },
-  k8s: {
-    label: { node: { id: "recluster.io/id" } },
-  },
   user: {
     username: { validation: { maxLength: 64 } },
     password: {
@@ -70,5 +47,4 @@ export const config: Config = {
       },
     },
   },
-  nodePool: { controller: { name: "controllers" }, worker: { minNodes: 0 } },
 };

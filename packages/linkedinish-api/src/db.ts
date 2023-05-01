96bb4ab61e6b0e065c0692ce98051d5443771077
Eddie Naff <eddienaff@gmail.com>
2023-04-30 21:18:33 -0700

    Add old db for drizzle ORM

--- packages/linkedinish-api/src/db.ts
+++ packages/linkedinish-api/src/db.ts
@@ -1,11 +1,19 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
+import pino from "pino";
 
 import { config } from "./config";
+import { logger } from "./logger";
 
-const pool = new Pool({
+export const pool = new Pool({
   //   connectionString: "postgres://user:password@host:port/db",
   connectionString: config.database.url,
 });
 
-export const db = drizzle(pool);
+pool.on("error", pgPoolError);
+
+export const db = drizzle(pool, { logger });
+
+function pgPoolError(err: unknown) {
+  logger.error(err, "Pool error");
+}
